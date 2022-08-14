44e495645b9e0281a50b26d87a6aeee9b0d1edf3
asrul10 <hanafi.asrul@kotaxdev.com>
2022-08-14 15:21:11 +0700

    Custom checkbox

--- src/App.svelte
+++ src/App.svelte
@@ -108,16 +108,13 @@
 
   const completeHabit = (event) => {
     const { currentTarget } = event;
-    const { id } = currentTarget.dataset;
+    const { target } = currentTarget.dataset;
     for (let index = 0; index < habits.length; index++) {
       const habit = habits[index];
-      if (habit.id !== parseInt(id)) {
+      if (habit.id !== parseInt(target)) {
         continue;
       }
-      habits[index].complete = false;
-      if (currentTarget.checked) {
-        habits[index].complete = true;
-      }
+      habits[index].complete = !habits[index].complete;
       break;
     }
     saveHabits(habits);
@@ -137,15 +134,20 @@
 
   {#each habits as habit}
     <div class="mb-2 flex items-start gap-x-2">
-      <input
-        class="w-5 h-5 rounded cursor-pointer mr-1 mt-1"
-        type="checkbox"
-        name="habit-{habit.id}"
-        id="habit-{habit.id}"
-        checked={habit.complete}
-        data-id={habit.id}
+      <span
+        class="w-5 h-5 rounded mt-1 p-1 mr-1 cursor-pointer {habit.complete
+          ? `bg-blue-500`
+          : `bg-white`}"
+        data-target={habit.id}
         on:click={completeHabit}
-      />
+      >
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+          <path
+            fill="#fff"
+            d="M438.6 105.4C451.1 117.9 451.1 138.1 438.6 150.6L182.6 406.6C170.1 419.1 149.9 419.1 137.4 406.6L9.372 278.6C-3.124 266.1-3.124 245.9 9.372 233.4C21.87 220.9 42.13 220.9 54.63 233.4L159.1 338.7L393.4 105.4C405.9 92.88 426.1 92.88 438.6 105.4H438.6z"
+          /></svg
+        >
+      </span>
       <span
         id="habit-label-{habit.id}"
         data-id={habit.id}
@@ -159,7 +161,8 @@
     </div>
   {/each}
 
-  <div>
+  <div class="mb-2 flex items-start gap-x-2">
+    <span class="w-5 h-5 mr-1" />
     <span
       on:keyup={addHabit}
       on:keypress={preventEnter}
@@ -172,7 +175,7 @@
   <div class="mt-8">
     <select
       name="year"
-      class="mb-5 font-bold text-2xl pr-2 rounded bg-transparent focus:outline-none"
+      class="mb-5 font-bold text-1xl pr-2 rounded bg-transparent focus:outline-none"
       id="report-year"
       bind:value={selectedMonth}
       on:change={changeMonth}
