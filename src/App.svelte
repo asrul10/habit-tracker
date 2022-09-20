b1f894c1aa597e7844e9f0795d76932c965f9f11
asrul10 <hanafi.asrul@kotaxdev.com>
2022-09-20 15:56:14 +0700

    add github link

--- src/App.svelte
+++ src/App.svelte
@@ -1,32 +1,24 @@
 <script>
   import HabitList from "./lib/HabitList.svelte";
   import seedingIcon from "./assets/seedling.png";
-
-  let focused = false;
-
-  window.addEventListener("focus", () => {
-    focused = true;
-  });
-
-  window.addEventListener("blur", () => {
-    focused = false;
-  });
 </script>
 
 <main class="text-white max-w-2xl px-4 mx-auto pt-8 pb-16">
-  <div
-    class={`flex items-center gap-2 mb-10 ${
-      focused ? "" : "h-screen justify-center"
-    }`}
-  >
-    <div>
-      <img src={seedingIcon} alt="habit" class="w-9 h-9" />
+  <div class="flex justify-between">
+    <div class={`flex items-center gap-2 mb-10`}>
+      <div>
+        <img src={seedingIcon} alt="habit" class="w-9 h-9" />
+      </div>
+      <h1 class="text-4xl font-bold">
+        <span class="text-lime-400">Habit</span> Tracker
+      </h1>
     </div>
-    <h1 class="text-4xl font-bold">
-      <span class="text-lime-400">Habit</span> Tracker
-    </h1>
+    <div>
+      <a
+        class="font-bold underline text-lime-400 hover:text-lime-300"
+        href="https://github.com/asrul10/habit-tracker">GitHub</a
+      >
+    </div>
   </div>
-  {#if focused}
-    <HabitList />
-  {/if}
+  <HabitList />
 </main>
