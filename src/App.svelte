b7a4fd05ba63c449ca2ee2454a992625ee992ccb
asrul10 <hanafi.asrul@kotaxdev.com>
2022-09-20 15:29:12 +0700

    refresh on focus window

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,24 @@
 <script>
   import HabitList from "./lib/HabitList.svelte";
   import seedingIcon from "./assets/seedling.png";
+
+  let focused = false;
+
+  window.addEventListener("focus", () => {
+    focused = true;
+  });
+
+  window.addEventListener("blur", () => {
+    focused = false;
+  });
 </script>
 
 <main class="text-white max-w-2xl px-4 mx-auto pt-8 pb-16">
-  <div class="flex items-center gap-2 mb-10">
+  <div
+    class={`flex items-center gap-2 mb-10 ${
+      focused ? "" : "h-screen justify-center"
+    }`}
+  >
     <div>
       <img src={seedingIcon} alt="habit" class="w-9 h-9" />
     </div>
@@ -12,6 +26,7 @@
       <span class="text-lime-400">Habit</span> Tracker
     </h1>
   </div>
-
-  <HabitList />
+  {#if focused}
+    <HabitList />
+  {/if}
 </main>
